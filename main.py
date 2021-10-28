63fa89585574a25c077815e85a79012f82464167
Ryan <74478729+hootloot@users.noreply.github.com>
2021-10-27 22:11:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,36 +17,42 @@ async def on_ready():
   #opens json file
   f = open('main.json')
   data = json.load(f)
+  m_index = 0
   for i in data["overall"]:
     #iter in the json, sections of overall
     #opens channel and path every ilter
     channel = client.get_channel(742148980682784793)
     videoList = os.system(folder) #for json; do videoList = os.listdir(i['name']) 
-
-    m_index = 0
-    for i in data['overall']:
-      m_index += 1 
-      video = i['path' + str(m_index)]
-
-      dur = i['dur' + str(m_index)]
-      desc = i['desc' + str(m_index)]
-      genre = i['genre' + str(m_index)]
-
-
-      target = os.path.join(videoList, video)
-      res_str = video.replace('.mp4', '')
-      await channel.send(res_str + ' is playing')
-      await channel.send(i['desc' + str(m_index)])
-      os.system('omxplayer "{}" > /dev/null'.format(target))
-      open_w = open('current.txt', 'r+')
-      #open txt file, then clears, then writes name of file name - .mp4
-      open_w.truncate(0)
-      open_w.write(res_str + '\n') 
-      open_w.write(dur)
-      open_w.write(desc)
-      open_w.write(genre)
+    m_index += 1 
+    video = i['path' + str(m_index)]
+    dur = i['dur' + str(m_index)]
+    desc = i['desc' + str(m_index)]
+    genre = i['genre' + str(m_index)]
+    target = os.path.join(videoList, video)
+    res_str = video.replace('.mp4', '')
+    await channel.send(res_str + ' is playing')
+    await channel.send(i['desc' + str(m_index)])
+    os.system('omxplayer "{}" > /dev/null'.format(target))
+    open_w = open('current.txt', 'r+')
+    #open txt file, then clears, then writes name of file name - .mp4
+    open_w.truncate(0)
+    open_w.write(res_str + '\n') 
+    open_w.write(dur)
+    open_w.write(desc)
+    open_w.write(genre)
       #video is playing, add commands below
 
+
+@client.command
+async def help(ctx,  member: discord.Member = None):
+  member = ctx.author if not member else member
+  embed = discord.Embed(color=0x5207df, timestamp=ctx.message.created_at, title="How To Use The Bot")
+  embed.set_footer(text=f"yeye, {ctx.author}", icon_url=ctx.author.avatar_url)
+  embed.set_footer(text = "More Commands Coming Soon!")
+  embed.add_field(name="**!movie**", value = "Information about the current moive playing")
+  embed.set_image(url= 'https://media.istockphoto.com/photos/people-in-the-cinema-auditorium-with-empty-white-screen-picture-id1131545834')
+
+  await ctx.send(embed=embed)
 
 @client.command
 async def movie(ctx, member: discord.Member = None):
@@ -62,16 +68,31 @@ async def movie(ctx, member: discord.Member = None):
   #Line2 = description
   #Line3 = genre
   member = ctx.author if not member else member
-  embed = discord.Embed(color=0x5207df, timestamp=ctx.message.created_at, title="Current Moive Playing")
+  Current_Movie = "Current Moive Playing: " + Lines[0]
+  embed = discord.Embed(color=0x5207df, timestamp=ctx.message.created_at, title=Current_Movie)
   embed.set_footer(text=f"Hello, {ctx.author}", icon_url=ctx.author.avatar_url)
-  embed.add_field(name="Movie Title", value = Lines[0], inline=False)
+  embed.set_footer(text="Showing Current Moive")
   embed.add_field(name="Duration", value = Lines[1], inline=False)
   embed.add_field(name="Description", value = Lines[2], inline=False)
   embed.add_field(name="Genre", value = Lines[3], inline=False)
   embed.set_image(url= 'https://media.istockphoto.com/photos/people-in-the-cinema-auditorium-with-empty-white-screen-picture-id1131545834')
   
-
   await ctx.send(embed=embed)
-  
+
+@client.command
+async def showings(ctx, member: discord.Member = None):
+  member = ctx.author if not member else member
+  embed = discord.Embed(color=0x5207df, timestamp=ctx.message.created_at, title="Moive Showings")
+  embed.set_footer(text=f"Hello, {ctx.author}", icon_url=ctx.author.avatar_url)
+  f = open('main.json')
+  data = json.load(f)
+  i_index = 0
+  for i in data["overall"]:
+    i_index += 1
+    embed.set_footer(text="The " + str(i_index) + "rd Movie")
+    embed.add_field(name=i['path'] + str(i_index), value = i['desc' + str(i_index)])
+    await ctx.send(embed=embed)
+
 
 client.run(TOKEN)
+
